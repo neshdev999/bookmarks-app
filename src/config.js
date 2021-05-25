05b94a3ef720fbd3c6b3e73c0a5ca92aafd66977
neshdev999 <71468718+neshdev999@users.noreply.github.com>
2021-05-25 07:16:23 -0400

    update config
    update

--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
 export default {
     // API_ENDPOINT: `https://tf-ed-bookmarks-api.herokuapp.com/v3/bookmarks`,
-    API_ENDPOINT: `http://localhost:8000/api/bookmarks`,
+    // API_ENDPOINT: `http://localhost:8000/api/bookmarks`,
+    API_ENDPOINT: `https://quiet-gorge-36937.herokuapp.com/`,
     API_KEY: process.env.REACT_APP_API_KEY
 }
